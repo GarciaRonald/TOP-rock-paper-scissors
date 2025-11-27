163c2a544dd3662a3fa4e0ec7e308e53dc281dba
Ronald Garcia <ronaldgm@gmail.com>
2025-11-27 18:51:03 -0500

    Move score vars and playRound func inside playGame func

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,6 +1,3 @@
-let humanScore = 0;
-let computerScore = 0;
-
 // Computer choice, return rock/paper/scissors based on random number
 const getComputerChoice = () => {
     let randNum = Math.floor(Math.random() * 3) + 1;
@@ -17,51 +14,56 @@ const getComputerChoice = () => {
 // Human choice, return user input
 const getHumanChoice = () => prompt('Enter Rock, Paper or Scissors:', 'Rock');
 
-// Play round, based on computer and human choice
-const playRound = (humanChoice, computerChoice) => {
-    let lowerCaseHumanChoice = humanChoice.toLowerCase();
 
-    if (lowerCaseHumanChoice === 'rock') {
-        if (computerChoice === 'Paper') {
-            computerScore++;
-            console.log('Computer wins! Paper beats Rock');
-        } else if (computerChoice === 'Scissors') {
-            humanScore++;
-            console.log('You win! Rock beats Scissors');
-        } else {
-            humanScore++;
-            computerScore++;
-            console.log('Tied! Both chose Rock');
-        }
-    } else if (lowerCaseHumanChoice === 'paper') {
-        if (computerChoice === 'Rock') {
-            humanScore++;
-            console.log('You win! Paper beats Rock');
-        } else if (computerChoice === 'Scissors') {
-            computerScore++;
-            console.log('You lose! Scissors beat Paper');
-        } else {
-            humanScore++;
-            computerScore++;
-            console.log('Tied! Both chose Paper');
-        }
-    } else if (lowerCaseHumanChoice === 'scissors') {
-        if (computerChoice === 'Rock') {
-            computerScore++;
-            console.log('You lose! Rock beats Scissors');
-        } else if (computerChoice === 'Paper') {
-            humanScore++;
-            console.log('You win! Scissors beat Paper');
-        } else {
-            humanScore++;
-            computerScore++;
-            console.log('Tied! Both chose Scissors');
-        }
-    }
-};
 
 // Play 5 rounds, display scores and declare winner
 const playGame = () => {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    // Play round, based on computer and human choice
+    const playRound = (humanChoice, computerChoice) => {
+        let lowerCaseHumanChoice = humanChoice.toLowerCase();
+
+        if (lowerCaseHumanChoice === 'rock') {
+            if (computerChoice === 'Paper') {
+                computerScore++;
+                console.log('Computer wins! Paper beats Rock');
+            } else if (computerChoice === 'Scissors') {
+                humanScore++;
+                console.log('You win! Rock beats Scissors');
+            } else {
+                humanScore++;
+                computerScore++;
+                console.log('Tied! Both chose Rock');
+            }
+        } else if (lowerCaseHumanChoice === 'paper') {
+            if (computerChoice === 'Rock') {
+                humanScore++;
+                console.log('You win! Paper beats Rock');
+            } else if (computerChoice === 'Scissors') {
+                computerScore++;
+                console.log('You lose! Scissors beat Paper');
+            } else {
+                humanScore++;
+                computerScore++;
+                console.log('Tied! Both chose Paper');
+            }
+        } else if (lowerCaseHumanChoice === 'scissors') {
+            if (computerChoice === 'Rock') {
+                computerScore++;
+                console.log('You lose! Rock beats Scissors');
+            } else if (computerChoice === 'Paper') {
+                humanScore++;
+                console.log('You win! Scissors beat Paper');
+            } else {
+                humanScore++;
+                computerScore++;
+                console.log('Tied! Both chose Scissors');
+            }
+        }
+    };
+
     playRound(getHumanChoice(), getComputerChoice());
     playRound(getHumanChoice(), getComputerChoice());
     playRound(getHumanChoice(), getComputerChoice());
