0dc9d8a3073dd7aa81bf7ab898cc7a00eb89c1c1
Ronald Garcia <ronaldgm@gmail.com>
2025-12-08 20:04:24 -0500

    Add constant for span placeholders

--- rock-paper-scissors-ui.js
+++ rock-paper-scissors-ui.js
@@ -1,3 +1,8 @@
 const btnRock = document.querySelector('#rock');
 const btnPaper = document.querySelector('#paper');
 const btnScissors = document.querySelector('#scissors');
+
+const roundNum = document.querySelector('#round span');
+const humanScore = document.querySelector('#human-score span');
+const compuScore = document.querySelector('#computer-score span');
+const winner = document.querySelector('#results-msg span');
